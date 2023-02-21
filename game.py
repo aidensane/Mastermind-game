082dda85e97b61a6dc0258b454c3a794b60d853d
aidensane <98586059+aidensane@users.noreply.github.com>
2023-02-21 07:19:16 +0100

    Update game.py

--- game.py
+++ game.py
@@ -12,3 +12,23 @@ def generate_code():
         code.append(color)
     
     return code
+
+def guess_code():
+
+    while True:
+        guess = input("Guess: ").upper().split(" ")
+
+        if len(guess) != CODE_LENGTH:
+            print(f"You must guess {CODE_LENGTH} colors.")
+            continue
+        
+        for color in guess:
+            if color not in COLORS:
+                print(f"Invalid color {color}. Try again.")
+            else:
+                break
+
+        return guess
+    
+def ch
+        
